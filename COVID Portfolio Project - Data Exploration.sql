cea3f9cd690e38e31e650d02cac9487c96d6b78a
as <86642487+Aayush-Singh02@users.noreply.github.com>
2024-03-31 19:01:56 +0530

    Updated

--- COVID Portfolio Project - Data Exploration.sql
+++ COVID Portfolio Project - Data Exploration.sql
@@ -3,7 +3,7 @@
 -- Likelihood of dying if a person contracts covid in their country
 
 Select Location, date, total_cases,total_deaths, (total_deaths/total_cases)*100 as DeathPercentage
-From PortfolioProject..CovidDeaths
+From CovidDeaths
 Where location like 'India'
 and continent is not null 
 order by 1,2
@@ -13,15 +13,14 @@ order by 1,2
 -- Shows what percentage of population infected with Covid
 
 Select Location, date, Population, total_cases,  (total_cases/population)*100 as PercentPopulationInfected
-From PortfolioProject..CovidDeaths
+From CovidDeaths
 order by 1,2
 
 
 -- Countries with Highest Infection Rate compared to Population
 
 Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
-From PortfolioProject..CovidDeaths
---Where location like 'India'
+From CovidDeaths
 Group by Location, Population
 order by PercentPopulationInfected desc
 
@@ -29,7 +28,7 @@ order by PercentPopulationInfected desc
 -- Countries with Highest Death Count per Population
 
 Select Location, MAX(cast(Total_deaths as int)) as TotalDeathCount
-From PortfolioProject..CovidDeaths
+From CovidDeaths
 Where continent is not null 
 Group by Location
 order by TotalDeathCount desc
@@ -41,7 +40,7 @@ order by TotalDeathCount desc
 -- Showing contintents with the highest death count per population
 
 Select continent, MAX(cast(Total_deaths as int)) as TotalDeathCount
-From PortfolioProject..CovidDeaths
+From CovidDeaths
 Where continent is not null 
 Group by continent
 order by TotalDeathCount desc
@@ -51,7 +50,7 @@ order by TotalDeathCount desc
 -- GLOBAL NUMBERS
 
 Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
-From PortfolioProject..CovidDeaths
+From CovidDeaths
 where continent is not null 
 Group By date
 order by 1,2
@@ -64,8 +63,8 @@ order by 1,2
 Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
 , SUM(CONVERT(int,vac.new_vaccinations)) OVER (Partition by dea.Location Order by dea.location, dea.Date) as RollingPeopleVaccinated
 --, (RollingPeopleVaccinated/population)*100
-From PortfolioProject..CovidDeaths dea
-Join PortfolioProject..CovidVaccinations vac
+From CovidDeaths dea
+Join CovidVaccinations vac
 	On dea.location = vac.location
 	and dea.date = vac.date
 where dea.continent is not null 
